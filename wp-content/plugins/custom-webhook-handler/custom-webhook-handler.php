b86a6e5a748b8ad1278c8efc007167c0d793a25e
victor20211220 <victor20211220@gmail.com>
2024-05-30 03:44:28 +0900

    dosn't show auto-reply messages.

--- wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
+++ wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
@@ -277,7 +277,7 @@ function send_message_to_endpoint($url, $message)
 
 function periskope_get_messages(WP_REST_Request $request)
 {
-    global $wpdb, $table_name;
+    global $wpdb, $table_name, $autoreply_message_body;
     // Set headers to prevent caching
     header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
     header('Cache-Control: post-check=0, pre-check=0', false);
@@ -301,7 +301,7 @@ function periskope_get_messages(WP_REST_Request $request)
         "SELECT a.*, b.message_id as quote_message_id, b.author as quote_author, b.body as quote_body, b.has_media as quote_has_media, b.media as quote_media
         FROM $table_name as a
         LEFT JOIN $table_name as b ON a.quoted_message_id = b.message_id
-        WHERE ISNULL(a.is_deleted) $hashtagFilter
+        WHERE ISNULL(a.is_deleted) AND a.body != '$autoreply_message_body' $ $hashtagFilter
         ORDER BY a.timestamp DESC
         LIMIT %d OFFSET %d",
         $limit,
