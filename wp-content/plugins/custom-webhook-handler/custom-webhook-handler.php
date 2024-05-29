9f6546f3066329f4d3f9a1a280e00ee939f31dc2
victor20211220 <victor20211220@gmail.com>
2024-05-29 14:13:18 +0900

    test webhook

--- wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
+++ wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
@@ -8,13 +8,17 @@ Author: Your Name
 
 // Hook to initialize our webhook handling
 add_action('rest_api_init', function () {
-    register_rest_route('webhook/v1', '/receive', array(
+    register_rest_route('webhook/v1', '/message_created', array(
         'methods' => 'POST',
-        'callback' => 'handle_webhook',
+        'callback' => 'handle_message_created',
+    ));
+    register_rest_route('webhook/v1', '/message_updated', array(
+        'methods' => 'POST',
+        'callback' => 'handle_message_updated',
     ));
 });
 
-function handle_webhook(WP_REST_Request $request) {
+function handle_message_created(WP_REST_Request $request) {
     // Retrieve the parameters from the request
     $parameters = $request->get_params();
 
@@ -26,5 +30,21 @@ function handle_webhook(WP_REST_Request $request) {
     }
 
     // Return a response
-    return new WP_REST_Response('Webhook received', 200);
+    return new WP_REST_Response('Message create webhook received', 200);
+}
+
+
+function handle_message_updated(WP_REST_Request $request) {
+    // Retrieve the parameters from the request
+    $parameters = $request->get_params();
+
+    // Process the parameters as needed
+    // For example, you can log them, save them to the database, etc.
+    // Here, we're just logging the data
+    if (!empty($parameters)) {
+        error_log(print_r($parameters, true));
+    }
+
+    // Return a response
+    return new WP_REST_Response('Message create webhook received', 200);
 }
