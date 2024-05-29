4eb73654cbd5936fe66c58b2191ba39f42f4f9c5
victor20211220 <victor20211220@gmail.com>
2024-05-29 14:20:13 +0900

    test webhook

--- wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
+++ wp-content/plugins/custom-webhook-handler/custom-webhook-handler.php
@@ -16,6 +16,10 @@ add_action('rest_api_init', function () {
         'methods' => 'POST',
         'callback' => 'handle_message_updated',
     ));
+    register_rest_route('webhook/v1', '/message_deleted', array(
+        'methods' => 'POST',
+        'callback' => 'handle_message_deleted',
+    ));
 });
 
 function handle_message_created(WP_REST_Request $request) {
@@ -35,6 +39,22 @@ function handle_message_created(WP_REST_Request $request) {
 
 
 function handle_message_updated(WP_REST_Request $request) {
+    // Retrieve the parameters from the request
+    $parameters = $request->get_params();
+
+    // Process the parameters as needed
+    // For example, you can log them, save them to the database, etc.
+    // Here, we're just logging the data
+    if (!empty($parameters)) {
+        error_log(print_r($parameters, true));
+    }
+
+    // Return a response
+    return new WP_REST_Response('Message create webhook received', 200);
+}
+
+
+function handle_message_deleted(WP_REST_Request $request) {
     // Retrieve the parameters from the request
     $parameters = $request->get_params();
 
